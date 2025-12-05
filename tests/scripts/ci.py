8601ca73c11a00fc4a1498d221026bde5bcdfc15
B, Siva Rama Krishna Reddy <sivb@qti.qualcomm.com>
2025-12-05 21:09:29 +0530

    Enable nvidia texture tests. (#168)
    Co-authored-by: Siva <quic_sivb@quicinc.com>

--- tests/scripts/ci.py
+++ tests/scripts/ci.py
@@ -601,6 +601,7 @@ generated = [
                 [
                     "./tests/scripts/task_opencl_cpp_unittest.sh {build_dir}",
                     "./tests/scripts/task_python_unittest.sh",
+                    "./tests/scripts/task_python_unittest_gpuonly.sh",
                 ],
             ),
             "frontend": ("run frontend tests", ["./tests/scripts/task_python_frontend.sh"]),
