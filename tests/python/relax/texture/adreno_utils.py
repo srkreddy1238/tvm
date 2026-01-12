45c872196686e8ab9fcd984ccb77569ada449f2c
Krishnaa, Sanjay <sanjs@qti.qualcomm.com>
2026-01-12 21:17:05 +0530

    Refactored adreno_utils.py (#172)
    Current Implementation of RPC doesn't implement RAII based destruction
    and can cause CI hangups if there's an issue between remote creation and
    destruction.
    
    This PR implementation ensures rpc connection gets closed even if
    there's any hiccups during evaluation.

--- tests/python/relax/texture/adreno_utils.py
+++ tests/python/relax/texture/adreno_utils.py
@@ -19,8 +19,9 @@
 import os
 
 import numpy as np
-
 import tvm
+import tempfile
+
 import tvm.testing
 from tvm import relax
 from tvm.contrib import dlpack as dl
@@ -30,6 +31,36 @@ from tvm.rpc import connect_tracker
 from tvm.script.parser import ir as I
 from tvm.script.parser import relax as R
 from tvm.script.parser import tir as T
+
+
+class RemoteConnection:
+    """
+    RPC class handling creation/destruction of remote connection.
+    """
+
+    def __init__(self):
+        self.RPC_TRACKER_HOST = os.getenv("TVM_TRACKER_HOST", "localhost")
+        self.RPC_TRACKER_PORT = int(os.getenv("TVM_TRACKER_PORT", 7979))
+        self.RPC_KEY = os.getenv("RPC_DEVICE_KEY", "android")
+
+    def __enter__(self):
+        self.tracker = tvm.rpc.connect_tracker(self.RPC_TRACKER_HOST, self.RPC_TRACKER_PORT)
+        self.remote = self.tracker.request(self.RPC_KEY, priority=0, session_timeout=600)
+        return self.remote
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.remote.get_function("CloseRPCConnection")()
+
+
+def is_target_rpc():
+    """
+    Checks the system env if the target is a remote device
+
+    Returns
+    -------
+    bool: True if RPC_TARGET is set, False otherwise
+    """
+    return "RPC_TARGET" in os.environ
 
 
 def get_target(backend, is_adreno=False):
@@ -48,26 +79,6 @@ def get_target(backend, is_adreno=False):
         ("vulkan", True): "qcom/adreno-vulkan-texture",
     }
     return tvm.target.Target(_TAG_MAP[(backend, is_adreno)])
-
-
-def get_rpc():
-    """
-    Establish an RPC connection to the remote device.
-
-    Returns
-    -------
-    tvm.rpc.RPCSession or None
-        The RPC session object if RPC_TARGET is set; otherwise, None.
-    """
-    rpc_target = os.getenv("RPC_TARGET", None)
-    if rpc_target:
-        host = os.getenv("TVM_TRACKER_HOST", "localhost")
-        port = int(os.getenv("TVM_TRACKER_PORT", 9090))
-        device_key = os.getenv("RPC_DEVICE_KEY", "android")
-        tracker = connect_tracker(host, port)
-        return tracker.request(device_key, priority=1, session_timeout=1000)
-    else:
-        return None
 
 
 def get_unique_dso_lib():
@@ -117,7 +128,7 @@ def run_cpu(mod, inputs, save_lib=False):
 
 
 def build_run(mod, inputs, backend, is_adreno=False):
-    remote = get_rpc()
+    is_rpc = is_target_rpc()
     target = get_target(backend, is_adreno)
     if remote is None:
         tgt = tvm.target.Target(target, host="llvm")
@@ -128,58 +139,53 @@ def build_run(mod, inputs, backend, is_adreno=False):
     mod = relax_pipeline(mod)
     ex = tvm.compile(mod, tgt, tir_pipeline=tir_pipeline)
 
-    if remote is None:
-        # local execution
-        if "opencl" in backend:
-            dev = tvm.opencl(0)
-        elif "vulkan" in backend:
-            dev = tvm.vulkan(0)
-        else:
-            raise RuntimeError("Unsupported backend")
+    if is_rpc:
+        with RemoteConnection() as remote:
+            with tempfile.TemporaryDirectory() as temp_dir:
+                filename = get_unique_dso_lib()
+                file_path = os.path.join(temp_dir, filename)
+                ex.export_library(
+                    file_path, fcompile=ndk.create_shared, options=["-shared", "-fPIC", "-lm"]
+                )
 
+                remote.upload(file_path)
+                rexec = remote.load_module(filename)
+                dev = remote.device(str(target.kind))
+
+                if "vdevice" in mod.global_infos:
+                    device_arr = [dev for ii in range(len(mod.global_infos["vdevice"]))]
+                else:
+                    device_arr = [dev]
+
+                vm = relax.VirtualMachine(rexec, device_arr)
+
+                inputs = [tvm.runtime.tensor(inp, dev) for inp in inputs]
+                vm.set_input("main", *inputs)
+                vm.invoke_stateful("main")
+                tvm_output = vm.get_outputs("main")
+
+                if isinstance(tvm_output, tuple):
+                    tvm_output = (out.numpy() for out in tvm_output)
+                else:
+                    tvm_output = tvm_output.numpy()
+    else:
+        dev = tvm.device(str(target.kind))
         if "vdevice" in mod.global_infos:
             device_arr = [dev for ii in range(len(mod.global_infos["vdevice"]))]
         else:
             device_arr = [dev]
         vm = relax.VirtualMachine(ex, device_arr)
-    else:
-        # remote execution
-        temp = utils.tempdir()
-        filename = get_unique_dso_lib()
-        file_path = temp.relpath(filename)
-        ex.export_library(
-            file_path, fcompile=ndk.create_shared, options=["-shared", "-fPIC", "-lm"]
-        )
 
-        remote.upload(file_path)
-        rexec = remote.load_module(filename)
+        inputs = [tvm.runtime.tensor(inp, dev) for inp in inputs]
+        vm.set_input("main", *inputs)
+        vm.invoke_stateful("main")
+        tvm_output = vm.get_outputs("main")
 
-        if "opencl" in backend:
-            dev = remote.cl(0)
-        elif "vulkan" in backend:
-            dev = remote.vulkan(0)
+        if isinstance(tvm_output, tuple):
+            tvm_output = (out.numpy() for out in tvm_output)
         else:
-            raise RuntimeError("Unsupported backend")
+            tvm_output = tvm_output.numpy()
 
-        if "vdevice" in mod.global_infos:
-            device_arr = [dev for ii in range(len(mod.global_infos["vdevice"]))]
-        else:
-            device_arr = [dev]
-
-        vm = relax.VirtualMachine(rexec, device_arr)
-
-    inputs = [tvm.runtime.tensor(inp, dev) for inp in inputs]
-    vm.set_input("main", *inputs)
-    vm.invoke_stateful("main")
-    tvm_output = vm.get_outputs("main")
-
-    if isinstance(tvm_output, tuple):
-        tvm_output = (out.numpy() for out in tvm_output)
-    else:
-        tvm_output = tvm_output.numpy()
-
-    if remote:
-        remote.get_function("CloseRPCConnection")()
     return tvm_output
 
 
